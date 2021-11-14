cbd4e9c1a69c7d8b0f1db5903185e8aba08a9623
kavishka <kss20001218@gmail.com>
2021-11-14 22:26:59 +0530

    Add font color & size facility

--- photoEditor.py
+++ photoEditor.py
@@ -3,13 +3,14 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 
-def image_function(i,word):
+def image_function(i, word, font_size, font_color):
     name = str(i) + ".jpg"
     out_name = word + name
     image = Image.open(name)
-    font_type = ImageFont.truetype('Arial.ttf', 50)
+    font_type = ImageFont.truetype('Arial.ttf', font_size)
     draw = ImageDraw.Draw(image)
-    draw.text(xy=(10, 10), text=(word+str(i)), fill=(0, 0, 0), font=font_type, stroke_width=1, stroke_fill=(255, 255, 255))
+    draw.text(xy=(10, 10), text=(word + str(i)), fill=font_color, font=font_type, stroke_width=round(font_size/50),
+              stroke_fill=(255, 255, 255))
     image = image.save('converted/' + out_name)
     print("Image {} is done".format(i))
 
@@ -51,15 +52,30 @@ def set_prefix():
 
 
 imageCount = 0
+Colors = {"red": (181, 24, 32), "green": (12, 90, 16), "blue": (6, 8, 45), "black": (0, 0, 0)}
 file = open('report.txt', 'w+')
 start = int(input("Enter starting number : "))
 end = int(input("Enter ending number : "))
 pr = set_prefix()
 make_directory('converted')
-print('-> converted folder created successfully')
+print('--> converted folder created successfully')
+fontS = int(input("Enter Font Size (50px) : "))
+if fontS < 30:
+    print("--> Font too small\n--> Font size : 30 ")
+    fontS = 30
+else:
+    print("--> Font size : {} ".format(fontS))
+print('Colour list - red | green | blue | black')
+inpu = input("Enter Font Colour : ").strip()
+if inpu != "red" and inpu != "green" and inpu != "blue" and inpu != "black":
+    print("--> Font colour : black ")
+    fontC = (0, 0, 0)
+else:
+    print("--> Font colour : {} ".format(inpu))
+    fontC = Colors[inpu]
 for i in range(start, end + 1):
     try:
-        image_function(i, pr)
+        image_function(i, pr, fontS, fontC)
         imageCount += 1
     except:
         file.write("image {} has error \n".format(i))
